2821112b4803a1f4926a85e41a159e8d61905472
ritik1507 <112415038+Ritik1507@users.noreply.github.com>
2024-07-19 10:37:54 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pypdf import PdfReader
+  
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
@@ -11,8 +11,34 @@ from langchain_google_genai import ChatGooglrGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
+from QASystem.ingestion import get_pdf_text,get_text_chunks,get_vector_store
+from QASystem.retrievalandgeneration import user_input
 
 
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
